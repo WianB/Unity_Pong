990d1ff2e20fff7cf49a984d5ceba469eb2565d7
WianB <wianbotha@gmail.com>
2017-08-31 14:54:53 +1000

    Added basic paddle implementation

--- Pong/Assets/Paddle.cs
+++ Pong/Assets/Paddle.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour {
 
+	public float speed = 5f;
+
+	private Rigidbody2D rb;
+
+
 	// Use this for initialization
 	void Start () {
-		
+		rb = this.GetComponent<Rigidbody2D> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+
+		//Move paddle up or down.
+		rb.MovePosition (
+			new Vector2 (
+				rb.position.x, rb.position.y + (Input.GetAxis ("Vertical") * Time.deltaTime * speed)
+			)
+		);
 		
 	}
 }
